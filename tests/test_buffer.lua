df05f0518cc12a9a08912ce462558092215088f8
picalines <picalines0@gmail.com>
2026-04-05 20:59:32 +0300

    fix: keymap.set expects buf instead of buffer in 0.12

--- tests/test_buffer.lua
+++ tests/test_buffer.lua
@@ -1,6 +1,8 @@
 local new_set = MiniTest.new_set
+local eq = MiniTest.expect.equality
 local expect = MiniTest.expect
 
+local H = dofile(vim.fn.getcwd() .. '/tests/helpers.lua')
 local keymap = require 'mappet'
 
 local T = new_set()
@@ -20,6 +22,27 @@ T['buffer()']['fails on unloaded buffer'] = function()
   expect.error(function()
     keymap.buffer('unloaded-buffer', buf)
   end, 'buffer is not loaded')
+end
+
+T['buffer()']['creates buffer-local mappings'] = function()
+  local name = H.unique_name 'buffer-local'
+  local lhs = '<Plug>(mappet-buffer-local)'
+  local target_buf = vim.api.nvim_get_current_buf()
+  local other_buf = vim.api.nvim_create_buf(true, false)
+
+  keymap.buffer(name, target_buf) { 'n', silent = true } {
+    keymap.map(lhs, 'buffer only') '<Nop>',
+  }
+
+  local target_map = H.get_buffer_map(target_buf, 'n', lhs)
+  local other_map = H.get_buffer_map(other_buf, 'n', lhs)
+  local global_map = H.get_global_map('n', lhs)
+
+  eq(H.truthy(target_map), true)
+  eq(H.truthy(other_map), false)
+  eq(H.truthy(global_map), false)
+  eq(target_map.desc, 'buffer only')
+  eq(target_map.silent, 1)
 end
 
 return T
